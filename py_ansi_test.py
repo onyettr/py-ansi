47783d88fe1544bd2aa04e9e10d590b924f5473a
Richard Onyett <onyettr@gmail.com>
2023-07-10 19:39:31 -0700

    Add files via upload

---
+++ py_ansi_test.py
@@ -0,0 +1,24 @@
+
+from isp_print import *
+
+def main():
+
+    isp_print_color("blue", "Hello")
+    isp_print_terminal_reset()
+    isp_print_clear_screen()
+    isp_print_at_xy("red", "hello <10,1>", 10,1)
+    isp_print_at_xy("red", "hello <123,1>", 23,1)
+    isp_print_at_xy("green", "there <3,4>", 3,4)
+    isp_print_at_xy("cyan", "string3 <15,10>", 15,10)
+    isp_print_at_xy("yellow", "string2 <20,20>", 20,20)
+
+    input("Next tests")
+    isp_print_terminal_reset()
+    isp_print_clear_screen()
+
+    for rows in range(1,20):
+        for i in range(1,80):
+            isp_print_at_xy("green", i, i,rows)
+
+if __name__ == "__main__":
+    main()
